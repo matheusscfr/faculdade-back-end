edeaae3284cc71f8db2f3d94b071c52497b36583
Matheus Ferreira <matheusscfr@gmail.com>
2025-06-08 16:44:43 -0300

    ajustes na porta do .env.

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ async function bootstrap() {
     whitelist: true, 
     forbidNonWhitelisted: true,
   }));
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT ?? 3000}`);
+  });
 }
 bootstrap();
